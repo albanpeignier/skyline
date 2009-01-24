eea19666cedbdbdf029f51d99602cd824a9fced4
Alban Peignier <alban.peignier@free.fr>
2009-01-24 23:28:16 +0100

    Add location to contact display name

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,7 +3,7 @@ class ContactsController < ApplicationController
   def index
     @contacts = Highrise::Person.find(:all, :params => { :term => params[:name] }, :from => "/people/search.xml").collect do |person|
       person.contact_data.phone_numbers.collect do |number| 
-        Contact.new :name => person.name, :number => number.number
+        Contact.new :name => "#{person.name} (#{number.location})", :number => number.number
       end
     end.flatten
   end
