6d1b5c1fc04b1ed08ae34606b87da26b6fecd425
Alban Peignier <alban.peignier@dryade.net>
2009-02-14 20:12:32 +0100

    Use a reasonable polling interval

--- cruise_config.rb
+++ cruise_config.rb
@@ -17,6 +17,6 @@ Project.configure do |project|
   # project.build_command = 'build_my_app.sh'
 
   # Ping Subversion for new revisions every 5 minutes (default: 30 seconds)
-  # project.scheduler.polling_interval = 5.minutes
+  project.scheduler.polling_interval = 30.minutes
 
 end
